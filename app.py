3fa8a54451db560226aa00cd2d2babf826acbc13
ZubAnt <ya.zubarevanton@yandex.ru>
2017-09-08 01:43:57 +0300

    add calculate m

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from DefaultGenerator.default_generator import DefaultGenerator
 
 seq1 = [4, 5, 0, 0]
-seq2 = [1, 2, 3, 4]
+seq2 = [1, 2, 3, 4, 5, 6, 7, 8]
 seq3 = [4, 3, 3, 4]
 
 default_generator = DefaultGenerator(seq2)
+x_list = list()
 
-for i in range(10):
+for i in range(1000):
     x = default_generator.generate()
-    print(x, end="   ")
+    x_list.append(x)
+    print("x =", x)
 
-print()
+m = sum(x_list) / len(x_list)
+print("\nm =", m)
