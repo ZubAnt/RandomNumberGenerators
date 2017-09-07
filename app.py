79d389a505b734841c5bf707fd3faf8b6e684803
ZubAnt <ya.zubarevanton@yandex.ru>
2017-09-08 01:49:51 +0300

    test

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from DefaultGenerator.default_generator import DefaultGenerator
 seq1 = [4, 5, 0, 0]
 seq2 = [1, 2, 3, 4, 5, 6, 7, 8]
 seq3 = [4, 3, 3, 4]
+seq4 = [3, 9, 0, 6, 2, 5, 0, 0]
 
-default_generator = DefaultGenerator(seq2)
+
+default_generator = DefaultGenerator(seq4)
 x_list = list()
 
-for i in range(1000):
+for i in range(10):
     x = default_generator.generate()
     x_list.append(x)
     print("x =", x)
