bdc634747a2b8c2a7fc8d4a874030f49ca2cf4e3
ZubAnt <ya.zubarevanton@yandex.ru>
2017-09-10 21:46:46 +0300

    hot fix

--- app.py
+++ app.py
@@ -7,17 +7,29 @@ seq2 = [1, 2, 3, 4, 5, 6, 7, 8]
 seq3 = [4, 3, 3, 4]
 seq4 = [3, 9, 0, 6, 2, 5, 0, 0]
 
-mu = 24
+m = 24
 lamb = 123456789
 z0 = 11111111
+nu = 0
+
+
+# m = 4
+# lamb = 2
+# z0 = 11
+# nu = 5
+
+# m = 4
+# lamb = 5
+# z0 = 7
+# nu = 0
 
 default_generator = DefaultGenerator(seq2)
-congruent_generator = CongruentGenerator(mu, lamb, z0)
+congruent_generator = CongruentGenerator(m, lamb, z0, nu)
 
 default_list = list()
 congruent_list = list()
 
-for i in range(1000000):
+for i in range(10000):
     # default_val = default_generator.generate()
     # print("default_val =", default_val, end="; ")
     # default_list.append(default_val)
@@ -30,5 +42,5 @@ for i in range(1000000):
 m_congruent = sum(congruent_list) / len(congruent_list)
 
 # print("\nm_default =", m_default)
-print("m_congruent =", m_congruent)
+print("\nm_congruent =", m_congruent)
 
--- congruent/generator.py
+++ congruent/generator.py
@@ -3,17 +3,23 @@ import numpy as np
 
 class CongruentGenerator(object):
 
-    def __init__(self, mu: int, lamb: int, z0: int) -> None:
-        self._mu = mu
+    def __init__(self, m: int, lamb: int, z0: int, nu: int = 0) -> None:
+        self._m = m
         self._lamb = lamb
         self._z0 = z0
-        self._divider = 2 ** mu
+        self._divider = 2 ** m
+        self._nu = nu
 
     def generate(self) -> float:
-        zlamb = self._z0 * self._lamb
-        zlamb_binary = np.binary_repr(zlamb, width=self._mu)
-        z1_binary = zlamb_binary[len(zlamb_binary) - self._mu:]
+
+        zlamb = self._z0 * self._lamb + self._nu
+        zlamb_binary = np.binary_repr(zlamb, width=self._nu)
+
+        z1_binary = zlamb_binary[len(zlamb_binary) - self._m:]
         self._z0 = int(z1_binary, base=2)
+
+        # print(f'zlamb = {zlamb}; zlamb_binary = {zlamb_binary}; z1_binary = {z1_binary}; z1 = {self._z0};')
+
         return float(self._z0 / self._divider)
 
 
